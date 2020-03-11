2479886cee13006d1f6f7e90ae92da6429f0f0bf
arj1211 <arjun.bawa@hotmail.com>
2020-03-10 23:36:07 -0400

    Added prettyDate function to print start/end times clean up + function and styles refactoring

--- src/Auth.js
+++ src/Auth.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {creds} from './creds.js';
+import { creds } from './creds.js';
 
 const CLIENT_ID = creds.CLIENT_ID;
 const API_KEY = creds.API_KEY;
@@ -13,6 +13,13 @@ class Auth extends React.Component {
         authed: false,
         events: null
     }
+    buttonStyle = {
+        width: '100px',
+        height: '100px',
+        backgroundColor: this.state.authed ? '#4CAF50' : '#45AEA0',
+        color: 'white',
+        fontSize: 'x-large'
+    }
     componentDidMount() {
         gapi.load('client:auth2', () => {
             gapi.client.init({
@@ -21,103 +28,138 @@ class Auth extends React.Component {
                 discoveryDocs: DISCOVERY_DOCS,
                 scope: SCOPES
             }).then(() => {
-                // Listen for sign-in state changes.
-                // console.log('IN THEN');
                 gapi.auth2.getAuthInstance().isSignedIn.listen((isSignedIn) => {
                     this.setState({ authed: isSignedIn })
-                    // console.log('JUST SET AUTHED TO: ' + this.state.authed + ' INSIDE LISTEN');
-                    // if (this.state.authed)
-                        this.listUpcomingEvents();
+                    this.listUpcomingEvents();
                 });
-                // Handle the initial sign-in state.
                 this.setState({ authed: gapi.auth2.getAuthInstance().isSignedIn.get() })
-                // console.log('JUST SET AUTHED TO: ' + this.state.authed);
                 if (this.state.authed)
                     this.listUpcomingEvents();
             }, (error) => {
-                // console.log(JSON.stringify(error, null, 2));
             });
         }
         );
     }
-    handleAuthIn() {
+
+    handleAuthIn = () =>
         gapi.auth2.getAuthInstance().signIn();
-        // console.log('JUST SIGNED IN?!');
-        // console.log(gapi.auth2.getAuthInstance().isSignedIn.get());
-    }
-    handleAuthOut() {
+
+    handleAuthOut = () =>
         gapi.auth2.getAuthInstance().signOut();
-        // console.log('JUST SIGNED OUT?!');
-        // console.log(gapi.auth2.getAuthInstance().isSignedIn.get());
-    }
+
     listUpcomingEvents() {
-        // console.log('JUST CALLED LISTEVENTSTHING');
         gapi.client.calendar.events.list({
             'calendarId': cal_id,
             'timeMin': (new Date()).toISOString(),
             'showDeleted': false,
             'singleEvents': true,
-            'maxResults': 10,
+            // 'maxResults': 10,
             'orderBy': 'startTime'
         }).then((response) => {
-            var events = response.result.items;
+            let events = response.result.items;
             this.setState({ events: events });
-            // console.log('THE EVENTS: ' + JSON.stringify(this.state.events));
         }, (error) => {
-            this.setState({events: null});
+            this.setState({ events: null });
         });
     }
+
+    prettyDate(d) {
+        let suffix = '';
+        switch (d.getDate()) {
+            case 1:
+                suffix = 'st';
+                break;
+            case 21:
+                suffix = 'st';
+                break;
+            case 31:
+                suffix = 'st';
+                break;
+            case 2:
+                suffix = 'nd';
+                break;
+            case 22:
+                suffix = 'nd';
+                break;
+            case 3:
+                suffix = 'rd';
+                break;
+            case 23:
+                suffix = 'rd';
+                break;
+            default:
+                suffix = 'th';
+                break;
+        }
+        suffix += ' ';
+        let mnth = '';
+        switch (d.getMonth() + 1) {
+            case 1:
+                mnth = 'Jan';
+                break;
+            case 2:
+                mnth = 'Feb';
+                break;
+            case 3:
+                mnth = 'Mar';
+                break;
+            case 4:
+                mnth = 'Apr';
+                break;
+            case 5:
+                mnth = 'May';
+                break;
+            case 6:
+                mnth = 'Jun';
+                break;
+            case 7:
+                mnth = 'Jul';
+                break;
+            case 8:
+                mnth = 'Aug';
+                break;
+            case 9:
+                mnth = 'Sep';
+                break;
+            case 10:
+                mnth = 'Oct';
+                break;
+            case 11:
+                mnth = 'Nov';
+                break;
+            default:
+                mnth = 'Dec';
+                break;
+        }
+        mnth += ' ';
+        return d.getDate() + suffix + mnth + d.getFullYear();
+    }
+
     render() {
         return (
             <div>
                 <h1>Auth Component</h1>
-                <button
-                    disabled={this.state.authed}
-                    style={
-                        {
-                            width: '100px',
-                            height: '100px',
-                            backgroundColor: '#45AEA0',
-                            color: 'white',
-                            fontSize: 'x-large',
-                            visibility: this.state.authed ? 'hidden' : 'visible'
-                        }
-                    }
-                    onClick={() => this.handleAuthIn()}>
-                    Sign In
+                <button style={this.buttonStyle}
+                    onClick={() => { this.state.authed ? this.handleAuthOut() : this.handleAuthIn() }}>
+                    Sign {this.state.authed ? 'Out' : 'In'}
                 </button>
-                <button
-                    disabled={!this.state.authed}
-                    style={
-                        {
-                            width: '100px',
-                            height: '100px',
-                            backgroundColor: '#4CAF50',
-                            color: 'white',
-                            fontSize: 'x-large',
-                            visibility: !this.state.authed ? 'hidden' : 'visible'
-                        }
-                    }
-                    onClick={() => this.handleAuthOut()}>
-                    Sign Out
-                </button>
-                <ol>
-                    {
-                        this.state.events ?
-                            this.state.events.map(ev =>
-                                <li key={ev.htmlLink}>
-                                    {
-                                        ev.summary
-                                        + ", from " +
-                                        (ev.start.dateTime || ev.start.dateTime)
-                                        + " to " +
-                                        (ev.end.dateTime || ev.end.dateTime)
-                                    }
-                                </li>)
-                            : ''
+                <ol style={{ textAlign: 'left' }}>
+                    {this.state.events ?
+                        this.state.events.map(
+                            ev => {
+                                let start = new Date(ev.start.dateTime || ev.start.date);
+                                let end = new Date(ev.end.dateTime || ev.end.date);
+                                let start_str = ", from ";
+                                start_str += this.prettyDate(start) + ' ' + start.toLocaleTimeString();
+                                let end_str = " to ";
+                                end_str += this.prettyDate(end) + ' ' + end.toLocaleTimeString();
+                                let date = start_str + end_str;
+                                return (<li key={ev.htmlLink}>{ev.summary + date}</li>)
+                            }
+                        )
+                        : ''
                     }
                 </ol>
-
             </div>
         );
     }
